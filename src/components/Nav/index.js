b2e8b01d811d3999df18b2317544edaf0103ea28
NicWille <nic.wille1020@yahoo.com>
2022-01-10 16:00:39 -0800

    conditionally render nav component

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,23 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
-function Nav () {
-    const categories = [
-        {
-          name: "commercial",
-          description:
-            "Photos of grocery stores, food trucks, and other commercial projects",
-        },
-        { name: "portraits", description: "Portraits of people in my life" },
-        { name: "food", description: "Delicious delicacies" },
-        {
-          name: "landscape",
-          description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-        },
-    ];
 
-    function categorySelected (name) {
-        console.log(`${name} section was clicked`)
-    }
+function Nav (props) {
+
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory,
+    } = props;
+
+    useEffect = (() => {
+        document.title = capitalizeFirstLetter(currentCategory.name)
+    }, [currentCategory])
+    // The second argument directs the hook to re-render the component on changes to the value of this state. In other words, if currentCategory changes now, the component will re-render so that the change in document.title will be visible to the user.
 
     return (
         <header>
@@ -35,9 +31,12 @@ function Nav () {
                         <span>Contact</span>
                     </li>
                     {categories.map((category) => (
-                        <li className="mx-1"  key={category.name}>
-                            <span onClick={() => categorySelected(category.name)}>
-                                {category.name}
+                        <li 
+                            className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`} 
+                            key={category.name}>
+                            <span
+                                onClick={() => {setCurrentCategory(category)}}>
+                                {capitalizeFirstLetter(category.name)}
                             </span>
                         </li>
                     ))}
